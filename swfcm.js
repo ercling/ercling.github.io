3b027a49da4af95749bd7978aa900bffde8784d1
Oleksandr Mykhailiuta <mykhailiuta.aleksandr@corp.mailfire.io>
2017-11-17 14:47:26 +0200

    init

---
+++ swfcm.js
@@ -0,0 +1,30 @@
+self.addEventListener('push', function (event) {
+  const data = event.data.json()
+  data.notification.data = data.data
+  data.notification.data.url = data.notification.click_action
+
+  event.waitUntil(self.registration.showNotification(data.notification.title, data.notification))
+  // Track open
+  fetch('https://api.mailfire.io/v1/webpush/show/' + data.data.id, {
+    method: 'post'
+  })
+})
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close()
+
+  // Show page
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: 'window'
+      })
+      .then(function() {
+        return clients.openWindow(event.notification.data.url)
+      })
+  )
+  // Track click
+  fetch('https://api.mailfire.io/v1/webpush/click/' + event.notification.data.id, {
+    method: 'post'
+  })
+})
